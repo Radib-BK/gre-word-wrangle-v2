70c68dd1c7512b88a04a553e540aa604825ddca8
Radib Bin Kabir <radibkabir41@gmail.com>
2024-07-26 21:47:16 +0600

    Revert "btn loading"
    This reverts commit dc0b1554787c1394fb6cd3e0dda3f68fd7d846bc.

--- app/page.jsx
+++ app/page.jsx
@@ -1,72 +1,28 @@
-'use client'
-
-import { useState } from 'react';
 import styles from './styles/Home.module.css';
 import Link from 'next/link';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 import Typed from '@/components/Typed';
 import BackgroundMusic from '@/components/BackGroundMusic';
-import ProfileButton from '@/components/profileButton';
+import ProfileButton from '@/components/profileButton'; //fix
 
 export default function Home() {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState({
-    game: false,
-    instructions: false
-  });
-
-  const handleClick = (route) => {
-    setIsLoading({ ...isLoading, [route]: true });
-    router.push(`/${route}`);
-  };
-
   return (
     <div className={styles.container}>
       <BackgroundMusic />
       <div className="absolute top-8 right-6">
-        <ProfileButton />
+      <ProfileButton />
       </div>
       <h1 id="mainHead" className="animate__animated animate__bounceInDown">
         GRE Word Wrangle
       </h1>
       <Typed />
       <div className={styles.buttons}>
-  <button 
-    className={styles.homeBtn} 
-    onClick={() => handleClick('game')}
-    disabled={isLoading.game}
-  >
-    {isLoading.game ? (
-      <Image 
-        src="/btnloading.gif" 
-        alt="Loading" 
-        width={35} 
-        height={30} 
-        className="mx-auto" // Centers the image horizontally
-      />
-    ) : (
-      'Play Game'
-    )}
-  </button>
-  <button 
-    className={styles.homeBtn} 
-    onClick={() => handleClick('instructions')}
-    disabled={isLoading.instructions}
-  >
-    {isLoading.instructions ? (
-      <Image 
-        src="/btnloading.gif" 
-        alt="Loading" 
-        width={35} 
-        height={30}
-        className="mx-auto" // Centers the image horizontally
-      />
-    ) : (
-      'Instructions'
-    )}
-  </button>
-</div>
+        <Link href="/game">
+          <button className={styles.homeBtn}>Play Game</button>
+        </Link>
+        <Link href="/instructions">
+          <button className={styles.homeBtn}>Instructions</button>
+        </Link>
+      </div>
     </div>
   );
 }
