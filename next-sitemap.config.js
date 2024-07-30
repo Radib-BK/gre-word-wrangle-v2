f46e789bd6eafc47ad036cae8ca368b9b2d5b553
Radib Bin Kabir <radibkabir41@gmail.com>
2024-07-30 14:48:16 +0600

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,9 @@
 module.exports = {
     siteUrl: 'https://wordwrangle.vercel.app', // Replace with your actual domain
     generateRobotsTxt: true, // Generate robots.txt file
-    sitemapSize: 5000, // Maximum entries per sitemap file
+    robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+    },
     changefreq: 'weekly', // Frequency of page changes
     priority: 0.7, // Default priority of URLs
   }
