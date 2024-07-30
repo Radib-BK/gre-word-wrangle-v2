dbce77a659fda1315d7c9e078418288d7abdf75c
Radib Bin Kabir <radibkabir41@gmail.com>
2024-07-30 13:57:42 +0600

    sitemap added

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://wordwrangle.vercel.app/', // Replace with your actual domain
+    generateRobotsTxt: true, // Generate robots.txt file
+    sitemapSize: 5000, // Maximum entries per sitemap file
+    changefreq: 'weekly', // Frequency of page changes
+    priority: 0.7, // Default priority of URLs
+  }
+  
